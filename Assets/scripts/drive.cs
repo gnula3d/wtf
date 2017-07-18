5b182d1de3b8663c6f395646dad0d893a16c982d
gnula3d <gnula3d@gmail.com>
2017-07-19 01:09:48 +0200

    tr

---
+++ Assets/scripts/drive.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine.Networking;
+
+public class drive : NetworkBehaviour {
+	public float speed = 10.0F;
+	public float rotationSpeed = 100.0F;
+
+
+	
+	void Update () {
+
+		float translation = CrossPlatformInputManager.GetAxis ("Vertical") * speed;
+		float roltation = CrossPlatformInputManager.GetAxis ("Horizontal") * rotationSpeed;
+
+		translation *= Time.deltaTime;
+		roltation *= Time.deltaTime;
+		transform.Translate (0, 0, translation);
+		transform.Rotate (0, rotationSpeed, 0);
+
+	}
+}
---
+++ Assets/scripts/wwwSprite.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class wwwSprite : MonoBehaviour {
+
+	// The output of the image
+	public Image img;
+
+	// The source image
+	public string url = "http://images.earthcam.com/ec_metros/ourcams/fridays.jpg";
+
+	IEnumerator Start() {
+		WWW www = new WWW(url);
+		yield return www;
+		img.sprite = Sprite.Create(www.texture, new Rect(0, 0, www.texture.width, www.texture.height), new Vector2(0, 0));
+	}
+}
